28f3b5d9e52cb7850501e9f35f8f5d97910c8478
Cicero Feijo <cicero.feijo@hp.com>
2021-01-13 19:50:45 -0300

    PHP: recebendo dados de um formulario

---
+++ form-com-php/processa.php
@@ -0,0 +1,11 @@
+<?php
+
+// criamos variaveis locais para receber os dados vindos do formulario
+$curso = $_POST['curso'];
+$estado = $_POST['estado'];
+
+echo 'Curso digitado: ' . $curso;
+echo '<br>';
+echo 'Estado:' . $estado;
+
+?>
